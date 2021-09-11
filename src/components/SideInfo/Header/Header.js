f05db101dfdc77cfa7fedc5bf95d02397d29a134
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-09-11 11:26:43 -0300

    Add language to Header.js

--- src/components/SideInfo/Header/Header.js
+++ src/components/SideInfo/Header/Header.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
 import photo from "./fotocurriculum.jpg";
 
-function Header() {
+const options = {
+  ES: "Desarrollador Web",
+  EN: "Web Developer",
+};
+
+function Header({ lang }) {
+  const [ocupation, setOcupation] = useState(options.ES);
+
+  useEffect(() => {
+    setOcupation(options[lang]);
+  }, [lang]);
   return (
     <>
       <section className="header box">
         <h1>Pablo Donalisio</h1>
-        <p id="profesion">Desarrolador Web</p>
+        <p id="profesion">{ocupation}</p>
         <img id="photo" src={photo} alt="Foto de Pablo Donalisio" />
       </section>
     </>

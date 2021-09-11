a2ed9e7576c13a690fed4143bef9e27c0eb3a9bd
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-09-11 11:25:47 -0300

    Add language to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import SideInfo from "./components/SideInfo/SideInfo";
 import Main from "./components/Main/Main";
 
 function App() {
+  const [language, setLanguage] = useState("ES");
   return (
     <div className="App">
-      <SideInfo />
-      <Main />
+      <SideInfo lang={language} />
+      <Main lang={language} />
     </div>
   );
 }

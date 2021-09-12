0188e95bd2193cd2a6a25d7159bed73c5e09ac4d
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-09-12 09:02:52 -0300

    Add types to skills

--- src/components/Main/Skills/Data.js
+++ src/components/Main/Skills/Data.js
@@ -1,38 +1,71 @@
-export const data = [
-  {
-    name: "HTML5",
-    file: "html5.png",
-  },
-  {
-    name: "CSS3",
-    file: "CSS3.svg",
-  },
-  {
-    name: "JavaScript",
-    file: "javascript.png",
-  },
-  {
-    name: "ReactJs",
-    file: "React.png",
-  },
-  {
-    name: "Ruby",
-    file: "ruby.jpeg",
-  },
-  {
-    name: "Rails",
-    file: "rails.png",
-  },
-  {
-    name: "PostgreSQL",
-    file: "postgresql.jpeg",
-  },
-  {
-    name: "Git",
-    file: "git.png",
-  },
-  {
-    name: "Github",
-    file: "github.png",
-  },
-];
+export const data = {
+  skills: [
+    {
+      name: "HTML5",
+      file: "html5.png",
+      type_id: 1,
+    },
+    {
+      name: "CSS3",
+      file: "CSS3.svg",
+      type_id: 1,
+    },
+    {
+      name: "JavaScript",
+      file: "javascript.png",
+      type_id: 1,
+    },
+    {
+      name: "ReactJs",
+      file: "React.png",
+      type_id: 2,
+    },
+    {
+      name: "Ruby",
+      file: "ruby.jpeg",
+      type_id: 1,
+    },
+    {
+      name: "Rails",
+      file: "rails.png",
+      type_id: 2,
+    },
+    {
+      name: "PostgreSQL",
+      file: "postgresql.jpeg",
+      type_id: 3,
+    },
+    {
+      name: "Git",
+      file: "git.png",
+      type_id: 4,
+    },
+    {
+      name: "Github",
+      file: "github.png",
+      type_id: 4,
+    },
+  ],
+  types: [
+    {
+      id: 1,
+      ES: "Lenguajes de programación",
+      EN: "Programming language",
+    },
+    {
+      id: 2,
+      ES: "Framework",
+      EN: "Framework",
+    },
+    {
+      id: 3,
+      ES: "Base de datos",
+      EN: "Database",
+    },
+    {
+      id: 4,
+      ES: "Control de versiones",
+      EN: "Version control system",
+    },
+  ],
+};
--- src/components/Main/Skills/Skills.js
+++ src/components/Main/Skills/Skills.js
@@ -5,12 +5,13 @@ import { LanguageContext } from "../../../LanguageContext";
 
 function Skills() {
   const lang = useContext(LanguageContext);
+  const skills = data.skills.sort((a, b) => (a.type_id < b.type_id ? -1 : 1));
   return (
     <>
       <section className="skills box">
         <h2>{lang === "ES" ? "Herramientas" : "Skills"}</h2>
         <div>
-          {data.map((skill, idx) => {
+          {skills.map((skill, idx) => {
             return (
               <figure key={idx}>
                 <img

4d962846636f6cd90f2f91a8aac471f3edcfaeb6
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-09-12 08:25:08 -0300

    Refactor Skills

---
+++ src/components/Main/Skills/Data.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    name: "HTML5",
+    file: "html5.png",
+  },
+  {
+    name: "CSS3",
+    file: "CSS3.svg",
+  },
+  {
+    name: "JavaScript",
+    file: "javascript.png",
+  },
+  {
+    name: "ReactJs",
+    file: "React.png",
+  },
+  {
+    name: "Ruby",
+    file: "ruby.jpeg",
+  },
+  {
+    name: "Rails",
+    file: "rails.png",
+  },
+  {
+    name: "PostgreSQL",
+    file: "postgresql.jpeg",
+  },
+  {
+    name: "Git",
+    file: "git.png",
+  },
+  {
+    name: "Github",
+    file: "github.png",
+  },
+];

6a652181ae873ea160dcc271989f159987754782
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-09-28 13:35:13 -0300

    Add Settings to hiddenElements

--- src/components/SettingsPanel/SettingsPanel.js
+++ src/components/SettingsPanel/SettingsPanel.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./SettingsPanel.css";
 import { useGlobalContext } from "../../context";
 import SettingsBox from "./SettingsBox";
 import { data } from "./Data";
 
 function SettingsPanel() {
-  const { lang, setLang } = useGlobalContext();
+  const { lang, setLang, setHiddenElements } = useGlobalContext();
   const [showSettings, setShowSettings] = useState(false);
   const [cvYOffset, setCvYOffset] = useState(0);
+  const settingsContainer = useRef("");
   useEffect(() => {
     document.addEventListener("scroll", () => {
       const cv = document.getElementById("curriculum");
@@ -15,8 +16,15 @@ function SettingsPanel() {
       yOffset < 0 ? setCvYOffset(Math.abs(yOffset)) : setCvYOffset(0);
     });
   }, []);
+  useEffect(() => {
+    setHiddenElements((prev) => [...prev, settingsContainer.current]);
+  }, [setHiddenElements]);
   return (
-    <div className="settings-container" style={{ top: cvYOffset }}>
+    <div
+      className="settings-container hidden"
+      style={{ top: cvYOffset }}
+      ref={settingsContainer}
+    >
       <button
         className="menu-btn"
         onClick={() => setShowSettings(!showSettings)}

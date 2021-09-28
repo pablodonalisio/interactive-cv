6aed2d2dcb12e91cc3aed2b5979ff5fd2e514715
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-09-28 17:00:56 -0300

    Create info modal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import SideInfo from "./components/SideInfo/SideInfo";
 import Main from "./components/Main/Main";
 import SettingsPanel from "./components/SettingsPanel/SettingsPanel";
+import Info from "./components/Info/Info";
 
 function App() {
   return (
     <div className="App">
+      <Info />
       <div id="curriculum">
         <SettingsPanel />
         <SideInfo />
